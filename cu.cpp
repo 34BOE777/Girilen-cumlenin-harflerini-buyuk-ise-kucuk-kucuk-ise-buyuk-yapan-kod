0c78ec9623f2e5ee7ccd77db276e9ead545413d9
34BOE777 <98213674+34BOE777@users.noreply.github.com>
2022-04-04 12:14:27 +0300

    Update cu.cpp

--- cu.cpp
+++ cu.cpp
@@ -6,21 +6,23 @@ int main()
 {
 
 	string cu;
+	
 	getline(cin,cu);
-	for (int i=0;i<cu.size();i++)
+	for (int i=0;i<cu.length();i++)//length yerine size() da kullanılabilir ama size(), bizlere o string arrayinde bulunan eemanların büyüklüğünü byte cinsinden hesapladığı için sağlıklı bir çözüm olmaz. onun yerine arrayin direk eleman sayısını döndüren lentgh fonksiynunu kullanmak daha sağlıklı olacaktır. 
 	{
 		if(isupper(cu[i]))
 		{
-			cu[i]=toupper(cu[i]);
+			cu[i]=tolower(cu[i]);
+		
 	}
 
 		else if (islower(cu[i]))
 		{
-			cu[i]=tolower(cu[i]);
+				cu[i]=toupper(cu[i]);
 		}
 
 	}
-
 	cout<<cu<<endl;
+	
 	return 0;
 }
